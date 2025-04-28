461b80d7c4c4a773a7bb715a411b9fd2c332341f
ramG-reddy <venkataramgopal16@gmail.com>
2025-04-28 11:53:55 +0530

    init

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+oh-my-posh init pwsh --config 'C:\Users\Bommareddy\AppData\Local\Programs\oh-my-posh\themes\ramG.omp.json' | Invoke-Expression
+
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+set-PSReadLineOption -PredictionViewStyle ListView
---
+++ README.md
@@ -0,0 +1,5 @@
+## My dotfiles, In case I ever rage smash my Lappy :)
+
+.gitconfig -> Bruh, you live under a rock?
+extensions.json -> VSCode list of extensions I have
+ramG.omp.json -> Oh-my-posh promt style config
